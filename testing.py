120ad79d12434905bc051504ced5f697e0dff89b
Archit <archittyagi599@gmail.com>
2025-09-18 23:37:15 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import requests as re 
 
+# testing 
 print(
     re.get('https://y-chat-ai.onrender.com/status').content,
     re.get('https://y-chat-ai.onrender.com/video?id=TYEqenKrbaM').content ,
